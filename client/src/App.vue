77912b88434f222a892c385cd304c08ef61a6e8d
Jovan Shandro <jovan.shandro@identinet.io>
2021-09-20 15:10:27 +0200

    feat(client): initial setup

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+input[type="text"] {
+  margin-bottom: 20px;
+  padding: 10px;
+}
+</style>
---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Filter LearnVue Articles</h2>
+    <input v-model="query" type="text" placeholder="Filter Search" />
+    <br />
+    <button @click="reset">Reset</button>
+    <search-results :query="query" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import SearchResults from "./SearchResults.vue";
+export default {
+  components: {
+    SearchResults,
+  },
+  setup() {
+    const query = ref("");
+    const reset = () => {
+      query.value = ""; 
+    };
+
+    return {
+      reset,
+      query,
+    };
+  },
+};
+</script>
+
+<style>
+input[type="text"] {
+  margin-bottom: 20px;
+  padding: 10px;
+}
+</style>
