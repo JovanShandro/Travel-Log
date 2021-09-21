29c7722fb26119bac02ecc92f710e06344fbb97a
Jovan Shandro <jovan.shandro@identinet.io>
2021-09-21 09:08:12 +0200

    feat(server): setup typeorm with postgres.               Add user entity

---
+++ server/src/constants.ts
@@ -0,0 +1,2 @@
+export const PORT = process.env.PORT || 8000;
+export const __DEV__ = process.env.NODE_ENV === 'development';
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,22 @@ config();
 
 import 'module-alias/register';
 import app from '@/app';
+import { PORT } from '@/constants';
+import { createConnection } from 'typeorm';
+import { User } from '@/entities/User';
 
-const PORT = process.env.PORT || 8000;
+const main = async () => {
+  await createConnection({
+    synchronize: true,
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    logging: true,
+    entities: [User],
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+};
+
+main();
