118d82fa7871871586ef2cfb241bd202b7c27be0
Jovan Shandro <jovan.shandro@identinet.io>
2021-09-21 14:46:57 +0200

    feat(client): switch to vue-cli vue 3 template

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,7 +2,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'eslint-config-prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    'eslint-config-prettier',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
   rules: {
     'vue/no-unused-vars': 'error',
   },
--- client/.prettierrc.js
+++ client/.prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   semi: true,
-  trailingComma: 'all',
+  trailingComma: "all",
   singleQuote: true,
-  printWidth: 150,
+  printWidth: 100,
   tabWidth: 2,
 };
